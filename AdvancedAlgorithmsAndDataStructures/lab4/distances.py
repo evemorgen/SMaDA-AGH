24baeebf2195211e9f8efc2563bcbf3d75ff5ef3
Patryk Gałczyński <galczynski.patryk@gmail.com>
2018-11-10 11:42:24 +0100

    some fixes

--- AdvancedAlgorithmsAndDataStructures/lab4/distances.py
+++ AdvancedAlgorithmsAndDataStructures/lab4/distances.py
@@ -23,7 +23,7 @@ def import_data(filename):
 def distance(a: Point, b: Point) -> float:
     print("POINTS")
     # https://math.stackexchange.com/questions/42640/calculate-distance-in-3d-space
-    return sqrt((a.x - b.x)**2 + (a.y - b.y)**2 + (a.z - b.z)**2)
+    return sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2 + (a.z - b.z) ** 2)
 
 
 @multimethod  # noqa: F811
@@ -125,11 +125,14 @@ def distance(e1: Edge, e2: Edge) -> float:
     print("EDGES")
     # http://geomalgorithms.com/a07-_distance.html#dist3D_Segment_to_Segment()
     SMALL_NUM = 0.00000001
-    u, v, w = Vector(e1.p2, e1.p1), Vector(e2.p2, e2.p1), Vector(e1.p1, e2.p1)
+    #u, v, w = Vector(e1.p2, e1.p1), Vector(e2.p2, e2.p1), Vector(e1.p1, e2.p1)
+    u, v, w = Vector(e1.p1, e1.p2), Vector(e2.p1, e2.p2), Vector(e2.p1, e1.p1)
     a, b, c, d, e = dot(u, u), dot(u, v), dot(v, v), dot(u, w), dot(v, w)
     D = a * c - b * b
     sc, sN, sD = D, D, D
     tc, tN, tD = D, D, D
+    #sN = b * e - c * d
+    #tN = a * e - b * d
 
     if D < SMALL_NUM:
         sN, sD = 0.0, 1.0
@@ -137,7 +140,7 @@ def distance(e1: Edge, e2: Edge) -> float:
     else:
         sN, tN = (b * e - c * d), (a * e - b * d)
         if sN < 0.0:
-            sN, tN, tD = 0.0, e, c
+            sN, tN, tD = z0.0, e, c
         elif sN > sD:
             sN, tN, tD = sD, e + b, c
     if tN < 0.0:
@@ -157,8 +160,8 @@ def distance(e1: Edge, e2: Edge) -> float:
         else:
             sN, sD = (-d + b), a
 
-    sc = sN / sD #0.0 if abs(sN) < SMALL_NUM else sN / sD
-    tc = tN / tD #0.0 if abs(tN) < SMALL_NUM else tN / tD
+    sc = sN / sD  # 0.0 if abs(sN) < SMALL_NUM else sN / sD
+    tc = tN / tD  # 0.0 if abs(tN) < SMALL_NUM else tN / tD
     dP = w + (u * sc) - (v * tc)
     return dP.length()
 
@@ -179,8 +182,10 @@ def distance(a: Edge, b: Face) -> float:
 
 if __name__ == '__main__':
     solids = import_data('solid_data.txt')
+    print()
 #    print(solids[0], solids[1])
 #    print(distance(solids[0], solids[1]))
+"""
     print("faces: ", distance(
         Face(Point(1, 0, 0), Point(0, 1, 0), Point(0, 0, 0)),
         Face(Point(100, 0, 0.13), Point(0, 100, 0.13), Point(-100, -100, 0.13))
@@ -189,3 +194,4 @@ if __name__ == '__main__':
         Edge(Point(1, 2, 3), Point(2, 4, 6)),
         Edge(Point(4, 4, 4), Point(5, 6, 7))
     ))
+"""
