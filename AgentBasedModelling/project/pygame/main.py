3853cad0bbd1c4c9bb01f64577313f5c7180b9ad
Patryk Gałczyński <galczynski.patryk@gmail.com>
2018-12-14 00:08:43 +0100

    add yaml config, add some paths

--- AgentBasedModelling/project/pygame/main.py
+++ AgentBasedModelling/project/pygame/main.py
@@ -1,50 +1,30 @@
 import pygame
+import yaml
 from math import pi, sin, cos, sqrt
 from copy import copy
 from random import choice
 
 
-def random_car():
-    return "pictures/car{}.png".format(choice(range(1, 6)))
-
-
-pygame.init()
-
-display_width = 800
-display_height = 600
-screen = pygame.display.set_mode((display_width, display_height))
-clock = pygame.time.Clock()
-
-car_image = pygame.image.load(random_car())
-background_image = pygame.transform.scale(pygame.image.load('pictures/background.png'), (display_width, display_height))
-screen.blit(background_image, [0, 0])
-
-path1 = [
-    (50, 325), #(100, 325),
-    (150, 325), #(200, 325),
-    (250, 325), #(300, 325),
-    (350, 325), #(400, 325), 
-    (450, 300), (470, 245), (470, 175), (470, 125),
-    (470, 75), (470, 25), (470, -25)
-]
+def load_config(filename):
+    with open(filename, 'r') as yml_file:
+        return yaml.load(yml_file)
 
 
 class Car(pygame.sprite.Sprite):
-    def __init__(self, path, dir):
+    def __init__(self, config, path=None, dir=None, image=None):
         pygame.sprite.Sprite.__init__(self)
-        self.image = car_image
+        self.image = pygame.image.load(image or choice(config["car"]["sprites"]))
         self.rect = self.image.get_rect()
-        image_x, image_y = car_image.get_rect().size
         self.oryginal = copy(self.image)
-        self.dir = 0
-        self.velocity = 5
-        self.waypoints = path
+        self.dir = dir or 0
+        self.velocity = config["car"]["velocity"]
+        self.waypoints = path or choice(config["car"]["paths"])
         self.waypoint_idx = 1
+        print(self.waypoints)
         self.current_waypoint = self.waypoints[1]
         self.rect.x, self.rect.y = self.waypoints[0]
-        self.rect.x -= car_image.get_rect().size[0] / 2
-        self.rect.y -= car_image.get_rect().size[1] / 2
-        self.turn(dir)
+        self.rect.x -= self.image.get_rect().size[0] / 2
+        self.rect.y -= self.image.get_rect().size[1] / 2
 
     def next_waypoint(self):
         self.waypoint_idx += 1
@@ -63,11 +43,9 @@ class Car(pygame.sprite.Sprite):
             self.current_waypoint[0] - self.rect.x - self.image.get_rect()[0],
             self.current_waypoint[1] - self.rect.center[1]
         )
-        current_heading = pygame.math.Vector2(self.velocity, self.velocity)
+        current_heading = pygame.math.Vector2(self.velocity, 0)
         current_heading.rotate_ip(self.dir)
-        #to_rotate = waypoint_vector.angle_to(current_heading)
         to_rotate = current_heading.angle_to(waypoint_vector)
-        print(waypoint_vector, current_heading, to_rotate, self.waypoint_idx)
         self.turn(to_rotate)
 
     def update(self):
@@ -78,8 +56,14 @@ class Car(pygame.sprite.Sprite):
             self.next_waypoint()
 
 
+pygame.init()
+config = load_config("simple.yaml")
+background_image = pygame.image.load(config["background"])
+screen = pygame.display.set_mode(background_image.get_rect()[2:])
+clock = pygame.time.Clock()
+screen.blit(background_image, [0, 0])
 all_sprites = pygame.sprite.Group()
-car = Car(path1, 90)
+car = Car(config, dir=-90)
 all_sprites.add(car)
 n = 1
 
@@ -91,12 +75,10 @@ while True:
 
     screen.blit(background_image, [0, 0])
     all_sprites.update()
-    #car.update()
 
     all_sprites.draw(screen)
     pygame.draw.rect(screen, (255, 0, 0), car.rect, 5)
-    for x, y in path1:
-        pygame.draw.rect(screen, (255, 0, 0), (x, y, 10, 10), 5)
-
+    for w in car.waypoints:
+        pygame.draw.rect(screen, (255, 0, 0), w + [5, 5], 2)
     pygame.display.flip()
-    clock.tick(5)
+    clock.tick(60)
