b7d9b60932745a7e17dc443348ee036009f85193
Patryk Gałczyński <galczynski.patryk@gmail.com>
2019-01-14 08:14:36 +0100

    ea labN first approach

---
+++ EvolutionaryAlgorithms/labN/moea-lab/moea-lab/src/main/java/Main.java
@@ -0,0 +1,31 @@
+import org.moeaframework.Executor;
+
+
+public class Main {
+
+
+    public static void ex1() {
+        Executor executor = new Executor();
+    }
+
+    public static void ex2() {
+
+    }
+
+    public static void ex3() {
+
+    }
+
+    public static void ex4() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello MOEA!");
+        ex1();
+        ex2();
+        ex3();
+        ex4();
+    }
+
+}
